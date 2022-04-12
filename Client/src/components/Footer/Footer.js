4e013afc33123e2ac86665710a0ffbdace74f0a2
shrouk khaled <shroukkhaled381@gmail.com>
2022-04-12 04:24:04 +0200

    upload data-file

--- Client/src/components/Footer/Footer.js
+++ Client/src/components/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../../css/Footer/Footer.css'
+import {words} from '../../words'
 
 function Footer() {
     return(
         <footer>
-         &copy; CopyWrite Shrouk Khaled 2022
+         { words.footerTitle }
       </footer>
     )
 }
