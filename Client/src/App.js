a69bc05c6a2a29075bcd66756e66dbfb652d422b
shrouk khaled <shroukkhaled381@gmail.com>
2022-04-12 05:21:56 +0200

    upload products

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,15 +1,25 @@
+import React, {useState, useEffect} from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import {words} from "./words"
+import data from './data.json'
+import Products from "./components/Products/Products";
 
 
 function App() {
+  const [products, setProducts] = useState(data)
+
   return (
      <div className="layout">
         <Header/>
 
         <main>
-          { words.content }
+          <div className="wrapper">
+              <Products products={products}/>
+            <div className="filter">
+              Filter
+            </div>
+          </div>
         </main>
 
         <Footer/>
